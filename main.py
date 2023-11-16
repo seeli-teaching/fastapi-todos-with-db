7f76fd1a253e0df22ad4664ed83a1ccf9c66caad
Markus Seeli <markus@seeli.eu>
2023-11-16 15:15:33 +0100

    Working version

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import models
 
 models.Base.metadata.create_all(bind=engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
@@ -15,12 +16,12 @@ def get_db():
         db.close()
 
 
-
 app = FastAPI()
+
 
 @app.get("/")
 def index():
-    return {"Hello": "World"}
+    return {"Hello": "FastAPI"}
 
 
 # Get all todos
@@ -29,13 +30,16 @@ def get_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     todos = repository.get_todos(db, skip=skip, limit=limit)
     return todos
 
+
 # Get one todo
 @app.get("/todos/{todo_id}")
 def get_one_todo(todo_id: int, db: Session = Depends(get_db)):
     db_todo = repository.get_todo(db, todo_id=todo_id)
     if db_todo is None:
-        raise HTTPException(status_code=404, detail=f"Todo with id {todo_id} not found")
+        raise HTTPException(
+            status_code=404, detail=f"Todo with id {todo_id} not found")
     return db_todo
+
 
 # Create a todo
 @app.post("/todos")
@@ -45,19 +49,21 @@ def create_todo(todo: TodoItemCreate, db: Session = Depends(get_db)):
 
 # Update a todo
 @app.put("/todos/{todo_id}")
-def update_todo(todo_id : int, todo : TodoItemUpdate, db: Session = Depends(get_db)):
+def update_todo(todo_id: int, todo: TodoItemUpdate, db: Session = Depends(get_db)):
     db_todo = repository.get_todo(db, todo_id=todo_id)
     if db_todo is None:
-        raise HTTPException(status_code=404, detail=f"Todo with id {todo_id} not found")
-    
+        raise HTTPException(
+            status_code=404, detail=f"Todo with id {todo_id} not found")
+
     return repository.update_todo_item(db=db, todo_id=todo_id, todo=todo)
 
 
 # Delete a todo
 @app.delete("/todos/{todo_id}")
-def delete_todo(todo_id : int, db: Session = Depends(get_db)):
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
     db_todo = repository.get_todo(db, todo_id=todo_id)
     if db_todo is None:
-        raise HTTPException(status_code=404, detail=f"Todo with id {todo_id} not found")
+        raise HTTPException(
+            status_code=404, detail=f"Todo with id {todo_id} not found")
     repository.delete_todo_item(db=db, todo_id=todo_id)
     return {"Message": f"Todo with id {todo_id} successfully deleted"}
