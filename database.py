8c8f43c726ebbfa81c41789b58bcbfb63720ef8c
Markus Seeli <markus@seeli.eu>
2023-11-17 17:55:07 +0100

    Added pymysql

--- database.py
+++ database.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+# URL for MySQL: mysql+pymysql://{user}:{password}@{host}:3306/todo
 database_url = os.environ.get("DATABASE_URL", "sqlite:///./todo.db")
+
 
 engine = create_engine(database_url, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
- 
