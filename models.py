f5a4f2ab8219327ead37f1cb191968b70a73e503
Markus Seeli <markus@seeli.eu>
2023-11-17 20:06:11 +0100

    Sting Length Added

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Boolean, Column, Integer, String, DateTime, func
 from database import Base
 
+
 class Todo(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
+    title = Column(String(50))
+    description = Column(String(200))
     completed = Column(Boolean, default=False)
     created_at = Column(DateTime, server_default=func.now())
     completed_at = Column(DateTime, server_default=None, nullable=True)
